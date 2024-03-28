86a77273c80d6cbcf5dd6c736e13bab904b09005
Scarounine <paulinelesueur93@gmail.com>
2024-03-28 16:11:04 +0100

    test: createPerson test controller

---
+++ src/test/java/com/openclassrooms/safetyNet/controller/PersonControllerTest.java
@@ -0,0 +1,49 @@
+package com.openclassrooms.safetyNet.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.openclassrooms.safetyNet.model.Person;
+import com.openclassrooms.safetyNet.service.FirestationService;
+import com.openclassrooms.safetyNet.service.MedicalrecordService;
+import com.openclassrooms.safetyNet.service.PersonService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = PersonController.class)
+public class PersonControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @MockBean
+    PersonService personService;
+
+    @MockBean
+    FirestationService firestationService;
+
+    @MockBean
+    MedicalrecordService medicalrecordService;
+
+    @Test
+    public void testCreatePerson() throws Exception {
+        Person person = new Person(1L,"John", "Boyd", "1509 Culver St", "Culver", "97451", "841-874-6512", "jaboyd@email.com");
+        when(personService.savePerson(person)).thenReturn(person);
+        mockMvc.perform(post("/person")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(person)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.firstName", is("John")));
+    }
+
+}
