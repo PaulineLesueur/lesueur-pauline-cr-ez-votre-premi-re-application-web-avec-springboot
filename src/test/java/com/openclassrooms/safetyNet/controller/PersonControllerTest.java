e58184732eab6ea1dad451ab8b491b9ae29358b0
Scarounine <paulinelesueur93@gmail.com>
2024-03-28 16:46:30 +0100

    test: deleteExistingPerson test controller + refactor

--- src/test/java/com/openclassrooms/safetyNet/controller/PersonControllerTest.java
+++ src/test/java/com/openclassrooms/safetyNet/controller/PersonControllerTest.java
@@ -15,12 +15,12 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.util.Optional;
 
 import static javax.swing.UIManager.put;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.hamcrest.Matchers.is;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(controllers = PersonController.class)
 public class PersonControllerTest {
@@ -39,15 +39,26 @@ public class PersonControllerTest {
     @MockBean
     MedicalrecordService medicalrecordService;
 
+    private static Person person = new Person(1L,"John", "Boyd", "1509 Culver St", "Culver", "97451", "841-874-6512", "jaboyd@email.com");
+
     @Test
     public void testCreatePerson() throws Exception {
-        Person person = new Person(1L,"John", "Boyd", "1509 Culver St", "Culver", "97451", "841-874-6512", "jaboyd@email.com");
         when(personService.savePerson(person)).thenReturn(person);
         mockMvc.perform(post("/person")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(person)))
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("$.firstName", is("John")));
+    }
+
+    @Test
+    public void testDeleteExistingPerson() throws Exception {
+        when(personService.getPersonByLastnameAndFirstname(any(String.class), any(String.class))).thenReturn(Optional.of(person));
+        mockMvc.perform(delete("/person")
+                    .param("lastname", person.getLastName())
+                    .param("firstname", person.getFirstName()))
+                .andExpect(status().isOk())
+                .andExpect(content().string("John Boyd has been deleted successfully"));
     }
 
     @Test
